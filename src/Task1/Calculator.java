39ef691a84d475253907bc55b004088307a01df6
Vasiliy Kuznetsov <vasekku25@gmail.com>
2024-09-09 20:47:35 +0700

    Удалил лишний Main

--- src/Task1/Calculator.java
+++ src/Task1/Calculator.java
@@ -1,2 +1,24 @@
-package Task1;public class Calculator {
+package Task1;
+
+public class Calculator {
+
+    public static <T extends Number, U extends Number> double sum(T num1, U num2) {
+        return num1.doubleValue() + num2.doubleValue();
+    }
+
+    public static <T extends Number, U extends Number> double multiply(T num1, U num2) {
+        return num1.doubleValue() * num2.doubleValue();
+    }
+
+    public static <T extends Number, U extends Number> double divide(T num1, U num2) {
+        if (num2.doubleValue() == 0) {
+            throw new ArithmeticException("Что творишь? На ноль не делим!!!");
+        }
+        return num1.doubleValue() / num2.doubleValue();
+    }
+
+    public static <T extends Number, U extends Number> double subtract(T num1, U num2) {
+        return num1.doubleValue() - num2.doubleValue();
+    }
+
 }
--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -1,2 +1,15 @@
-package Task1;public class Main {
+package Task1;
+
+//1. Написать класс Калькулятор (необобщенный), который содержит обобщенные статические методы:
+//sum(), multiply(), divide(), subtract().
+//Параметры этих методов – два числа разного типа, над которыми должна быть произведена операция.
+//Методы должны возвращать результат своей работы.
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Сумма: " + Calculator.sum(10,5.5));
+        System.out.println("Умножение: " + Calculator.multiply(5.5,3));
+        System.out.println("Деление: " + Calculator.divide(8.2,0));
+        System.out.println("Разность: " + Calculator.subtract(9,5));
+    }
 }
